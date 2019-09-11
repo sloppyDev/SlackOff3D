36ca615c7d06472020f5155e729dbd70ac583252
Joe Selikoff <selikoff.j@gmail.com>
2019-09-10 23:49:43 -0500

    Showing two cubes with my two different scaleing approaches.  I think the new approach is good for now, but still will likely change for perspective.

--- Engine/Graphics/MeshManager.cpp
+++ Engine/Graphics/MeshManager.cpp
@@ -17,8 +17,8 @@ MeshManager::MeshManager()
    vector3 bottomColor = vector3(0.0f, 1.0f, 1.0f); // cyan
    vector3 leftColor = vector3(0.0f, 0.0f, 1.0f); // blue
    vector3 topColor = vector3(1.0f, 0.0f, 1.0f); // pink
-
-// Front 1
+   
+   // Front 1
    vector3 set1 = vector3(0.0f, 0.0f, 0.0f);
    vector3 set2 = vector3(0.0f, sideLength, 0.0f);
    vector3 set3 = vector3(sideLength, sideLength, 0.0f);
@@ -89,80 +89,77 @@ MeshManager::MeshManager()
    set2 = vector3(0.0f, sideLength, sideLength);
    set3 = vector3(sideLength, sideLength, sideLength);
    AddTri(triple(set1, set2, set3, topColor));
-   //// Front 1
-   //vector3 set1 = vector3(0.0f, 0.0f, 0.0f);
-   //vector3 set2 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
-   //vector3 set3 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
-   //AddTri(triple(set1, set2, set3, frontColor));
+   // Front 1
+   set1 = vector3(0.0f, 0.0f, 0.0f);
+   set2 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
+   set3 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
+   AddTri(triple(set1, set2, set3, frontColor));
 
-   //// Front 2
-   //set1 = vector3(0.0f, 0.0f, 0.0f);
-   //set2 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
-   //set3 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, 0.0f);
-   //AddTri(triple(set1, set2, set3, frontColor));
+   // Front 2
+   set1 = vector3(0.0f, 0.0f, 0.0f);
+   set2 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
+   set3 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, 0.0f);
+   AddTri(triple(set1, set2, set3, frontColor));
 
-   //// Right 1
-   //set1 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, 0.0f);
-   //set2 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
-   //set3 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
-   //AddTri(triple(set1, set2, set3, rightColor));
+   // Right 1
+   set1 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, 0.0f);
+   set2 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
+   set3 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
+   AddTri(triple(set1, set2, set3, rightColor));
 
-   //// Right 2
-   //set1 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, 0.0f);
-   //set2 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
-   //set3 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, sideLength / Engine::SCREEN_DEPTH);
-   //AddTri(triple(set1, set2, set3, rightColor));
+   // Right 2
+   set1 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, 0.0f);
+   set2 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
+   set3 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, sideLength / Engine::SCREEN_DEPTH);
+   AddTri(triple(set1, set2, set3, rightColor));
 
-   //// Back 1
-   //set1 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, sideLength / Engine::SCREEN_DEPTH);
-   //set2 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
-   //set3 = vector3(0.0f, 0.0f, sideLength / Engine::SCREEN_DEPTH);
-   //AddTri(triple(set1, set2, set3, backColor));
+   // Back 1
+   set1 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, sideLength / Engine::SCREEN_DEPTH);
+   set2 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
+   set3 = vector3(0.0f, 0.0f, sideLength / Engine::SCREEN_DEPTH);
+   AddTri(triple(set1, set2, set3, backColor));
 
-   //// Back 2
-   //set1 = vector3(0.0f, 0.0f, sideLength / Engine::SCREEN_DEPTH);
-   //set2 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
-   //set3 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
-   //AddTri(triple(set1, set2, set3, backColor));
+   // Back 2
+   set1 = vector3(0.0f, 0.0f, sideLength / Engine::SCREEN_DEPTH);
+   set2 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
+   set3 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
+   AddTri(triple(set1, set2, set3, backColor));
 
-   //// Bottom 1
-   //set1 = vector3(0.0f, 0.0f, 0.0f);
-   //set2 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, 0.0f);
-   //set3 = vector3(0.0f, 0.0f, sideLength / Engine::SCREEN_DEPTH);
-   //AddTri(triple(set1, set2, set3, bottomColor));
+   // Bottom 1
+   set1 = vector3(0.0f, 0.0f, 0.0f);
+   set2 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, 0.0f);
+   set3 = vector3(0.0f, 0.0f, sideLength / Engine::SCREEN_DEPTH);
+   AddTri(triple(set1, set2, set3, bottomColor));
 
-   //// Bottom 2
-   //set1 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, 0.0f);
-   //set2 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, sideLength / Engine::SCREEN_DEPTH);
-   //set3 = vector3(0.0f, 0.0f, sideLength / Engine::SCREEN_DEPTH);
-   //AddTri(triple(set1, set2, set3, bottomColor));
+   // Bottom 2
+   set1 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, 0.0f);
+   set2 = vector3(sideLength / Engine::SCREEN_WIDTH, 0.0f, sideLength / Engine::SCREEN_DEPTH);
+   set3 = vector3(0.0f, 0.0f, sideLength / Engine::SCREEN_DEPTH);
+   AddTri(triple(set1, set2, set3, bottomColor));
 
-   //// Left 1
-   //set1 = vector3(0.0f, 0.0f, 0.0f);
-   //set2 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
-   //set3 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
-   //AddTri(triple(set1, set2, set3, leftColor));
+   // Left 1
+   set1 = vector3(0.0f, 0.0f, 0.0f);
+   set2 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
+   set3 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
+   AddTri(triple(set1, set2, set3, leftColor));
 
-   //// Left 2
-   //set1 = vector3(0.0f, 0.0f, 0.0f);
-   //set2 = vector3(0.0f, 0.0f, sideLength / Engine::SCREEN_DEPTH);
-   //set3 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
-   //AddTri(triple(set1, set2, set3, leftColor));
+   // Left 2
+   set1 = vector3(0.0f, 0.0f, 0.0f);
+   set2 = vector3(0.0f, 0.0f, sideLength / Engine::SCREEN_DEPTH);
+   set3 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
+   AddTri(triple(set1, set2, set3, leftColor));
 
-   //// Top 1
-   //set1 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
-   //set2 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
-   //set3 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
-   //AddTri(triple(set1, set2, set3, topColor));
-   //triple TOP = triple(set1, set2, set3);
-   //vector3 normal = ComputeNormal(TOP);
-   //cout << normal.y << endl;
+   // Top 1
+   set1 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
+   set2 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
+   set3 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
+   AddTri(triple(set1, set2, set3, topColor));
 
-   //// Top 2
-   //set1 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
-   //set2 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
-   //set3 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
-   //AddTri(triple(set1, set2, set3, topColor));
+   // Top 2
+   set1 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, 0.0f);
+   set2 = vector3(0.0f, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
+   set3 = vector3(sideLength / Engine::SCREEN_WIDTH, (Engine::ASPECT_RATIO * sideLength) / Engine::SCREEN_HEIGHT, sideLength / Engine::SCREEN_DEPTH);
+   AddTri(triple(set1, set2, set3, topColor));
 }
 
 MeshManager::~MeshManager()
@@ -178,12 +175,21 @@ void MeshManager::AddTri(triple in)
 std::vector<triple> MeshManager::ScaleMesh()
 {
    std::vector<triple> scaledMesh;
+   int count = 0;
    for (auto&& tri : mesh)
    {
-      triple tri1 = tri / SCALE_VEC;
-      triple tri2 = tri1 * ASPECT_VEC;
+      if (count < 12)
+      {
+         triple tri1 = tri / SCALE_VEC;
+         triple tri2 = tri1 * ASPECT_VEC;
+         scaledMesh.push_back(tri2);
+      }
+      else
+      {
+         scaledMesh.push_back(tri);
+      }
+      count++;
 
-      scaledMesh.push_back(tri2);
    }
    return scaledMesh;
 }
@@ -220,7 +226,7 @@ bool MeshManager::InView(triple tri)
 }
 void MeshManager::Update()
 {
-   float angVel = 100.0f;
+   float angVel = 10.0f;
    float dt = Engine::dt;
    float rotAng = angVel * dt;
 
@@ -245,13 +251,23 @@ void MeshManager::Render()
    glLineWidth(2.5);
    glPointSize(2.5);
    glEnd();
-
+   int count = 0;
    for (auto&& tri : toRender)
    {
+      if (count < 12)
+      {
+         glColor4f(0, 1, 0, 1);
+      }
+      else
+      {
+         glColor4f(0, 0, 1, 1);
+      }
+      count++;
       normal = ComputeNormal(tri);
       glBegin(GL_LINES);
       {
-         glColor4f(1, 1, 1, 1);
+
+         //glColor4f(1, 1, 1, 1);
          glVertex2f(tri.p1.x, tri.p1.y);
          glVertex2f(tri.p2.x, tri.p2.y);
 
@@ -261,9 +277,9 @@ void MeshManager::Render()
          glVertex2f(tri.p3.x, tri.p3.y);
          glVertex2f(tri.p1.x, tri.p1.y);
 
-         glColor4f(tri.color.x, tri.color.y, tri.color.z, 1);
-         glVertex2f(normal.x + tri.center.x, normal.y + tri.center.y);
-         glVertex2f(tri.center.x, tri.center.y);
+         //glColor4f(tri.color.x, tri.color.y, tri.color.z, 1);
+         //glVertex2f(normal.x + tri.center.x, normal.y + tri.center.y);
+         //glVertex2f(tri.center.x, tri.center.y);
 
       }
       glEnd();
